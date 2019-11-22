3b175a5387f58659c861aa1940f74bf9475a6266
bakkeby <bakkeby@gmail.com>
2019-11-22 09:21:56 +0100

    Adding improvement to the fakefullscreenclient exiting out of proper fullscreen into tiled fullscreen

--- patch/fakefullscreenclient.c
+++ patch/fakefullscreenclient.c
@@ -10,6 +10,15 @@ togglefakefullscreen(const Arg *arg)
         else
             selmon->sel->isfullscreen = 0;
     } else {
+        if (selmon->sel->isfullscreen) {
+            selmon->sel->isfloating = selmon->sel->oldstate;
+            selmon->sel->bw = selmon->sel->oldbw;
+            selmon->sel->x = selmon->sel->oldx;
+            selmon->sel->y = selmon->sel->oldy;
+            selmon->sel->w = selmon->sel->oldw;
+            selmon->sel->h = selmon->sel->oldh;
+            resizeclient(selmon->sel, selmon->sel->x, selmon->sel->y, selmon->sel->w, selmon->sel->h);
+        }
         selmon->sel->fakefullscreen = 1;
         selmon->sel->isfullscreen = 0;
     }
