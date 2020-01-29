347ac5146cade7a6955c430fdf52556454283210
bakkeby <bakkeby@gmail.com>
2020-01-29 13:32:53 +0100

    Correcting warnings ref. #4

--- patch/fibonacci.h
+++ patch/fibonacci.h
@@ -1,3 +1,7 @@
+#if FIBONACCI_DWINDLE_LAYOUT
 static void dwindle(Monitor *m);
+#endif // FIBONACCI_DWINDLE_LAYOUT
 static void fibonacci(Monitor *m, int s);
+#if FIBONACCI_SPIRAL_LAYOUT
 static void spiral(Monitor *m);
+#endif // FIBONACCI_SPIRAL_LAYOUT
