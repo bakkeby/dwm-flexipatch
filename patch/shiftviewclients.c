a713e73a37d6374abd3556c3345eb8c6f88c0d69
bakkeby <bakkeby@gmail.com>
2020-06-05 11:24:07 +0200

    shiftviewclients: compatibility changes for scratchpads and scratchpad_alt_1 patches

--- patch/shiftviewclients.c
+++ patch/shiftviewclients.c
@@ -6,7 +6,15 @@ shiftviewclients(const Arg *arg)
 	unsigned int tagmask = 0;
 
 	for (c = selmon->clients; c; c = c->next)
+		#if SCRATCHPADS_PATCH
+		if (!(c->tags & SPTAGMASK))
+			tagmask = tagmask | c->tags;
+		#elif SCRATCHPAD_ALT_1_PATCH
+		if (!(c->tags & SCRATCHPAD_MASK))
+			tagmask = tagmask | c->tags;
+		#else
 		tagmask = tagmask | c->tags;
+		#endif // SCRATCHPADS_PATCH
 
 	shifted.ui = selmon->tagset[selmon->seltags];
 	if (arg->i > 0) // left circular shift
