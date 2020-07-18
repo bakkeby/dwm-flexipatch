aff7c0d8b8cd51b82df7d48c6db14af3353632cf
bakkeby <bakkeby@gmail.com>
2020-07-18 13:04:39 +0200

    Revert config.mk uncommenting

--- config.mk
+++ config.mk
@@ -21,7 +21,7 @@ FREETYPEINC = /usr/include/freetype2
 #FREETYPEINC = ${X11INC}/freetype2
 
 # Uncomment this for the alpha patch / BAR_ALPHA_PATCH
-XRENDER = -lXrender
+#XRENDER = -lXrender
 
 # Uncomment this for the mdpcontrol patch / MDPCONTROL_PATCH
 #MPDCLIENT = -lmpdclient
@@ -34,7 +34,7 @@ XRENDER = -lXrender
 #XEXTLIB = -lXext
 
 # Uncomment this for the swallow patch / SWALLOW_PATCH
-XCBLIBS = -lX11-xcb -lxcb -lxcb-res
+#XCBLIBS = -lX11-xcb -lxcb -lxcb-res
 
 # includes and libs
 INCS = -I${X11INC} -I${FREETYPEINC} ${PANGOINC}
