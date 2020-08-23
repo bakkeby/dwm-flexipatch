8ec80be7565e0b2e80a90ba617788b2d06a23f8d
bakkeby <bakkeby@gmail.com>
2020-08-23 14:32:59 +0200

    Fixing warp barmodules compatibility ref. #37

--- patch/warp.c
+++ patch/warp.c
@@ -13,8 +13,11 @@ warp(const Client *c)
 	     y > c->y - c->bw &&
 	     x < c->x + c->w + c->bw*2 &&
 	     y < c->y + c->h + c->bw*2) ||
-	    (y > c->mon->by && y < c->mon->by + bh) ||
-	    (c->mon->topbar && !y))
+	    x < c->mon->wx ||
+	    x > c->mon->wx + c->mon->ww ||
+	    y < c->mon->wy ||
+	    y > c->mon->wy + c->mon->wh
+	)
 		return;
 
 	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w / 2, c->h / 2);
