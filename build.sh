b2dc184e858995dbb826e9fecce53d005d9fa521
tiago.paranhos <tiagoprn@gmail.com>
2021-06-22 13:43:46 -0300

    feat: updated build script.

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ set -e
 
 notify-send -u critical 'Compiling and installing/updating dwm...' && \
 DESTDIR=~/local PREFIX='' make clean install && \
-sudo cp -farv ~/local/bin/dwm /bin/dwm && ls -lha /bin/dwm && \
+cp -farv dwm $DESTDIR/bin/dwm && ls -lha $DESTDIR/bin/dwm && \
+sudo cp -farv dwm /bin/dwm && ls -lha /bin/dwm && \
 notify-send -u critical "DWM will be restarted now..." && \
 pkill -HUP dwm
