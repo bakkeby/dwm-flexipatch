2c5f877bd20497b188b5859b2cd88ab11068b554
bakkeby <bakkeby@gmail.com>
2022-01-18 14:29:11 +0100

    Ignoring unused functions warnings.
    This is achieved by adding the -Wno-unused-function flag to the compiler.
    The warnings are suppressed to avoid confusion for users new to dwm.
    
    Removing the static declaration from the header files works too, but adds
    unnecessary data into the compiled object.

--- config.mk
+++ config.mk
@@ -58,7 +58,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS}  ${XRENDER} ${MPDCLIENT
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
 #CFLAGS   = -g -std=c99 -pedantic -Wall -O0 ${INCS} ${CPPFLAGS}
-CFLAGS   = -std=c99 -pedantic -Wall -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c99 -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS}
 
 # Solaris
