39fde74dfd48056094c4eddacd7baac9194e5c1e
bakkeby <bakkeby@gmail.com>
2022-06-17 16:58:47 +0200

    systray: allow systray icons to survive a restart by handing systray icons over to the root window before destroying the systray window

--- patch/bar_systray.c
+++ patch/bar_systray.c
@@ -111,6 +111,7 @@ removesystrayicon(Client *i)
 	for (ii = &systray->icons; *ii && *ii != i; ii = &(*ii)->next);
 	if (ii)
 		*ii = i->next;
+	XReparentWindow(dpy, i->win, root, 0, 0);
 	free(i);
 	drawbarwin(systray->bar);
 }
