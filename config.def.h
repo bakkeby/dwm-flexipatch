ab7d28ff0f747eff6abc0dc460523c2d5c8f067a
bakkeby <bakkeby@gmail.com>
2023-06-27 16:31:02 +0200

    bar border - allow for the border size to be explicitly set ref. #364

--- config.def.h
+++ config.def.h
@@ -7,6 +7,12 @@ static const int corner_radius           = 10;
 #else
 static const unsigned int borderpx       = 1;   /* border pixel of windows */
 #endif // ROUNDED_CORNERS_PATCH
+#if BAR_BORDER_PATCH
+/* This allows the bar border size to be explicitly set separately from borderpx.
+ * If left as 0 then it will default to the borderpx value of the monitor and will
+ * automatically update with setborderpx. */
+static const unsigned int barborderpx    = 0;  /* border pixel of bar */
+#endif // BAR_BORDER_PATCH
 static const unsigned int snap           = 32;  /* snap pixel */
 #if SWALLOW_PATCH
 static const int swallowfloating         = 0;   /* 1 means swallow floating windows by default */
--- patch/setborderpx.c
+++ patch/setborderpx.c
@@ -15,13 +15,15 @@ setborderpx(const Arg *arg)
 	int delta = 2 * (m->borderpx - prev_borderpx);
 
 	#if BAR_BORDER_PATCH
-	for (bar = m->bar; bar; bar = bar->next) {
-		bar->bh = bar->bh - 2 * bar->borderpx + 2 * m->borderpx;
-		bar->borderpx = m->borderpx;
+	if (!barborderpx) {
+		for (bar = m->bar; bar; bar = bar->next) {
+			bar->bh = bar->bh - 2 * bar->borderpx + 2 * m->borderpx;
+			bar->borderpx = m->borderpx;
+		}
+		updatebarpos(m);
+		for (bar = m->bar; bar; bar = bar->next)
+			XMoveResizeWindow(dpy, bar->win, bar->bx, bar->by, bar->bw, bar->bh);
 	}
-	updatebarpos(m);
-	for (bar = m->bar; bar; bar = bar->next)
-		XMoveResizeWindow(dpy, bar->win, bar->bx, bar->by, bar->bw, bar->bh);
 	#endif // BAR_BORDER_PATCH
 
 	for (c = m->clients; c; c = c->next) {
