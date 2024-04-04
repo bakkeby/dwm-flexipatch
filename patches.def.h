36b574eff68239db35df97035118b614cc79d307
SalahDin Rezk <salah2112004@gmail.com>
2024-04-04 16:51:13 +0200

    Add bar padding smart patch (#419)

--- patches.def.h
+++ patches.def.h
@@ -369,6 +369,12 @@
  */
 #define BAR_PADDING_VANITYGAPS_PATCH 0
 
+/* Smart bar padding patch that automatically adjusts the padding when there is
+ * only one client on the monitor. Works well with vanitygaps and barpadding
+ * patches.
+ */
+#define BAR_PADDING_SMART_PATCH 0
+
 /* This patch adds simple markup for status messages using pango markup.
  * This depends on the pango library v1.44 or greater.
  * You need to uncomment the corresponding lines in config.mk to use the pango libraries
