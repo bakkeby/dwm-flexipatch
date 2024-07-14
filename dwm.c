3bc91e187cceee26d04ed9eb43c64676eecf3295
bakkeby <bakkeby@gmail.com>
2024-07-14 14:26:45 +0200

    Removing debug print statement

--- dwm.c
+++ dwm.c
@@ -4102,7 +4102,6 @@ spawn(const Arg *arg)
 	if (arg->v == dmenucmd)
 		dmenumon[0] = '0' + selmon->num;
 	#endif // NODMENU_PATCH
-fprintf(stderr, "spawn running cmd:\n");
 
 	#if RIODRAW_PATCH
 	if ((pid = fork()) == 0)
