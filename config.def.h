fd0911c61c9f4894cc5f15e81afa5496811e3d9a
bakkeby <bakkeby@gmail.com>
2026-05-17 13:47:57 +0200

    underlinetags: adding option to draw the line above the tags ref. #323

--- config.def.h
+++ config.def.h
@@ -114,6 +114,7 @@ static const unsigned int ulinepad = 5;         /* horizontal padding between th
 static const unsigned int ulinestroke  = 2;     /* thickness / height of the underline */
 static const unsigned int ulinevoffset = 0;     /* how far above the bottom of the bar the line should appear */
 static const int ulineall = 0;                  /* 1 to show underline on all tags, 0 for just the active ones */
+static const int ulinetop = 0;                  /* 1 to show the underline above the tags, rather than under */
 #endif // BAR_UNDERLINETAGS_PATCH
 
 #if NAMETAG_PATCH
