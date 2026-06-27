b2c27b1c145f35499a030a8f861fc7ae84cfe63a
bakkeby <bakkeby@gmail.com>
2026-06-27 17:54:31 +0200

    holdbar - do not skip non-holdkey releases ref. #483

--- patch/bar_holdbar.c
+++ patch/bar_holdbar.c
@@ -15,26 +15,27 @@ void
 keyrelease(XEvent *e)
 {
 	Bar *bar;
-	if (XEventsQueued(dpy, QueuedAfterReading)) {
-		XEvent ne;
-		XPeekEvent(dpy, &ne);
+	if (e->xkey.keycode == XKeysymToKeycode(dpy, HOLDKEY)) {
+		if (XEventsQueued(dpy, QueuedAfterReading)) {
+			XEvent ne;
+			XPeekEvent(dpy, &ne);
 
-		if (ne.type == KeyPress && ne.xkey.time == e->xkey.time &&
-				ne.xkey.keycode == e->xkey.keycode) {
-			XNextEvent(dpy, &ne);
-			return;
+			if (ne.type == KeyPress && ne.xkey.time == e->xkey.time &&
+					ne.xkey.keycode == e->xkey.keycode) {
+				return;
+			}
 		}
-	}
-	if (e->xkey.keycode == XKeysymToKeycode(dpy, HOLDKEY) && selmon->showbar == 2) {
-		selmon->showbar = 0;
-		updatebarpos(selmon);
-		for (bar = selmon->bar; bar; bar = bar->next)
-			XMoveResizeWindow(dpy, bar->win, bar->bx, bar->by, bar->bw, bar->bh);
-		#if BAR_SYSTRAY_PATCH
-		if (!selmon->showbar && systray)
-			XMoveWindow(dpy, systray->win, -32000, -32000);
-		#endif // BAR_SYSTRAY_PATCH
-		arrange(selmon);
+		if (selmon->showbar == 2) {
+			selmon->showbar = 0;
+			updatebarpos(selmon);
+			for (bar = selmon->bar; bar; bar = bar->next)
+				XMoveResizeWindow(dpy, bar->win, bar->bx, bar->by, bar->bw, bar->bh);
+			#if BAR_SYSTRAY_PATCH
+			if (!selmon->showbar && systray)
+				XMoveWindow(dpy, systray->win, -32000, -32000);
+			#endif // BAR_SYSTRAY_PATCH
+			arrange(selmon);
+		}
 	}
 	#if COMBO_PATCH
 	combo = 0;
